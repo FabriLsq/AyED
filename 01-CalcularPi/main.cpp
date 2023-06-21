43c6871961b3993b51cb1caad3c349b772d96750
Fabricio Lassaque <flassaque@frba.utn.edu.ar>
2023-06-21 11:02:03 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -4,19 +4,21 @@
 using namespace std;
 
 int main(){
+    cout << setprecision(8);
 
-    int precision = 900000;
     double leibniz = 1;
-
-    for( int i=1; i < precision; ++i){
-        if(i%2 == 0){
-            leibniz += 1.0/(2.0*i+1.0);
-        }else{
-            leibniz -= 1.0/(2.0*i+1.0);
+    int i = 0;
+    
+        while(int((leibniz*4.0)*1000000) != 3141592){
+            ++i;
+            if(i%2 == 0){
+                leibniz += 1.0/(2.0*i+1.0);
+            }else{
+                leibniz -= 1.0/(2.0*i+1.0);
+            }
         }
-    }
 
-    cout << "Pi = " << setprecision(7) << leibniz*4.0 << endl;
+    cout << "Pi = " << leibniz*4.0 << endl;
 
     return 0;
 }
